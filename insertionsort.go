5cd079d191bf26ae308d7e9dd914ca1725dc006f
rootVIII <colleyloyejames@gmail.com>
2020-06-17 22:34:37 -0400

    add insertion sort

--- insertionsort.go
+++ insertionsort.go
@@ -4,16 +4,16 @@ package main
 func InsertionSort(arr []int) []int {
 	var i int
 	var j int
-	var key int
+	var tmp int
 
 	for i = 1; i < int(len(arr)); i++ {
-		key = arr[i]
+		tmp = arr[i]
 		j = i - 1
-		for j >= 0 && arr[j] > key {
+		for j >= 0 && arr[j] > tmp {
 			arr[j+1] = arr[j]
 			j--
 		}
-		arr[j+1] = key
+		arr[j+1] = tmp
 	}
 	return arr
 }
